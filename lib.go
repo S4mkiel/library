f6d0a4474a45f48a42919f010e1bb51a36abf21a
Roberto Filho <zferraz.rf@gmail.com>
2023-01-31 20:54:51 -0300

    Develop by Trainee

--- lib.go
+++ lib.go
@@ -13,32 +13,28 @@ type Book struct {
 	gorm.Model
 	Title  string `json:"title,omitempty" gorm:"not null"`
 	Author string `json:"author,omitempty" gorm:"not null"`
-	ISBN uint 		`json:"isbn,omitempty" gorm:"not null"`
+	ISBN string 	`json:"isbn,omitempty" gorm:"not null"`
 }
 
 func main() {
+	db := connect()
+	defer db.Close()
 	fmt.Println("Welcome to Library System")
 	fmt.Println("What do you want to do?\n1. Add a book\n2. Search a book or Delete\n3. Exit")
 	var choice int
 	fmt.Scanln(&choice)
 	switch choice {
+		
 	case 1:
-		fmt.Println("Add a book")
-		db, err := gorm.Open("sqlite3", "library.db")
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer db.Close()
-		if err := db.AutoMigrate(&Book{}).Error; err != nil {
-			log.Fatal(err)
-		}
 		var rootCmd = &cobra.Command{Use: "app"}
 		var createCmd = &cobra.Command{
 			Use: "create",
+			Short: "Create a new book",
 			Run: func(cmd *cobra.Command, args []string){
 				Title, _ := cmd.Flags().GetString("title")
 				Author, _ := cmd.Flags().GetString("author")
-				ISBN, _ := cmd.Flags().GetUint("isbn")
+				ISBN, _ := cmd.Flags().GetString("isbn")
+				db := connect()
 				tx := db.Begin()
 				if err := tx.Create(&Book{Title: Title, Author: Author, ISBN: ISBN}).Error; err != nil {
 					tx.Rollback()
@@ -52,30 +48,54 @@ func main() {
 		createCmd.MarkFlagRequired("title")
 		createCmd.Flags().String("author", "", "Author of the book")
 		createCmd.MarkFlagRequired("author")
-		createCmd.Flags().Uint("isbn", 0, "ISBN of the book")
+		createCmd.Flags().String("isbn", "", "ISBN of the book")
 		createCmd.MarkFlagRequired("isbn")
 		rootCmd.AddCommand(createCmd)
 		rootCmd.Execute()
+
 	case 2:
 		fmt.Println("Search a book or Delete a book")
-					
-	case 3:
-		fmt.Println("Exit")
-		for {
-			fmt.Println("Are you sure you want to exit? (y/n)")
-			var choice string
-			fmt.Scanln(&choice)
-			if choice == "y" {
-				fmt.Println("Thank you for using our system")
-				break
-			} else if choice == "n" {
-				fmt.Println("Welcome back")
-				main()
-				break
-			} else {
-				fmt.Println("Invalid choice")
-			}
+		var rootCmd = &cobra.Command{Use: "app"}
+		var searchCmd = &cobra.Command{
+			Use: "search",
+			Run: func(cmd *cobra.Command, args []string){
+				isbn, _ := cmd.Flags().GetString("isbn")
+				var book Book
+				db := connect()
+				if err := db.Where("isbn = ?", isbn).First(&book).Error; err != nil {
+					log.Fatal(err)
+				}
+				fmt.Println(book)
+			},
 		}
+		var deleteCmd = &cobra.Command{
+			Use: "delete",
+			Run: func(cmd *cobra.Command, args []string){
+				isbn, _ := cmd.Flags().GetString("isbn")
+				var book Book
+				db := connect()
+				if err := db.Where("isbn = ?", isbn).First(&book).Error; err != nil {
+					log.Fatal(err)
+				}
+				tx := connect().Begin()
+				if err := tx.Delete(&book).Error; err != nil {
+					tx.Rollback()
+					return
+				}
+				tx.Commit()
+				fmt.Println("Book deleted successfully")
+			},
+		}
+		searchCmd.Flags().String("isbn", "", "ISBN of the book")
+		searchCmd.MarkFlagRequired("isbn")
+		deleteCmd.Flags().String("isbn", "", "ISBN of the book")
+		deleteCmd.MarkFlagRequired("isbn")
+		rootCmd.AddCommand(searchCmd, deleteCmd)
+		rootCmd.Execute()
+
+	case 3:
+		fmt.Println("Thank you for using our system")
+
 	default:
 		fmt.Println("Invalid choice")
 		for {
@@ -93,4 +113,14 @@ func main() {
 			}
 		}
 	}
+}
+func connect() *gorm.DB{
+	db, err := gorm.Open("sqlite3", "books.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := db.AutoMigrate(&Book{}).Error; err != nil {
+		log.Fatal(err)
+	}
+	return db
 }
